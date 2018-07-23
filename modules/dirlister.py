58b636ca12bed48fda0dffa5789d3d0f91720ace
Luciano Carlos <luciano.krlos18@gmail.com>
2018-07-23 03:10:06 -0300

    update

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -4,7 +4,7 @@ import os
 import math
 
 def run(**args):
-	#print("[*] In dirlister module.")
+	print("[*] In dirlister module.")
 	files = os.listdir(".")
 	files.append(math.pow(2,2))
 	return str(files)
